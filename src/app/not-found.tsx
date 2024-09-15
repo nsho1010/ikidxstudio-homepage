b026ccbdd8aabaae214ac1e97d254a61be3eb28e
Shohei Nagamura <shohei.nagamura@gmail.com>
2024-09-15 22:19:31 +0900

    404（not-fount）ページを作成

---
+++ src/app/not-found.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="flex flex-col items-center justify-center gap-4 lg:gap-8 p-8 h-screen">
+      <h2 className="">404</h2>
+      <p>お探しのページは見つかりません</p>
+    </div>
+  );
+};
+
+export default NotFound;
